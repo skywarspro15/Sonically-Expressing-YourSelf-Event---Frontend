dc2834ab8f2bb53d03c59f3cf2a68fe36d493aa7
skwyarspro15 <potatobg648@gmail.com>
2023-11-20 22:42:47 +0800

    Added waiting room & song status

--- js/index.js
+++ js/index.js
@@ -34,6 +34,43 @@ function replaceText(data) {
   presInteraction.appendChild(presentationText);
 }
 
+function waitingScreen(data) {
+  replaceText(data);
+  let actions = [
+    {
+      text: "Set a nickname",
+      actions: "alert('testing1')",
+    },
+    {
+      text: "See what song is currently playing",
+      actions: "alert('testing2');",
+    },
+    {
+      text: "Interact on the show booth",
+      actions: "alert('testing3')",
+    },
+  ];
+
+  let pollContainer = document.createElement("div");
+  pollContainer.className = "poll";
+
+  actions.forEach((action) => {
+    let actionButton = document.createElement("button");
+    actionButton.innerText = action.text;
+    actionButton.className = "poll-option";
+    actionButton.addEventListener("click", () => {
+      eval(action.actions);
+    });
+    pollContainer.appendChild(actionButton);
+  });
+
+  presInteraction.appendChild(pollContainer);
+}
+
+socket.on("waiting", (data) => {
+  waitingScreen(data);
+});
+
 socket.on("text", (data) => {
   replaceText(data);
 });
